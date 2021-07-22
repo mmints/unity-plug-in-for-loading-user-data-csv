1a4ab6e17b53085dbb0afade089ae739116e74c2
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-07-22 17:29:01 +0200

    [Impl] Create an Object that hold the user data from a CSV file

--- LoadUserDataCSV.cs
+++ LoadUserDataCSV.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using System.IO;
+using System.Linq;
+using UnityEngine.UIElements;
+
+public class UserData
+{
+    private float TimeStamp { get; set; }
+    private Vector3 Position { get; set; }
+    private Vector3 LookAt { get; set; }
+    
+    public UserData(float timeStamp, Vector3 position, Vector3 lookAt)
+    {
+        TimeStamp = timeStamp;
+        Position = position;
+        LookAt = lookAt;
+    }
+}
 
 public class LoadUserDataCSV : EditorWindow
 {
     private string debugMessage = "";
-    
+    private string pathToCSV;
+
+    private UserData[] userData = new UserData[0];
+
     // *** BEGIN GUI LAYOUT *** //
     
     [MenuItem("Window/Load User Data CSV")] // Add menu item to the Window menu
@@ -15,9 +35,35 @@ public class LoadUserDataCSV : EditorWindow
         // Show existing window instance. If one doesn't exist, make one.
         EditorWindow.GetWindow(typeof(LoadUserDataCSV));
     }
-
+    
     void OnGUI()
     {
+        pathToCSV = EditorGUILayout.TextField ("Path to CSV", pathToCSV);
+
+        if (GUILayout.Button("Load"))
+        {
+            string data = File.ReadAllText(pathToCSV);
+            string[] rows = data.Split('\n');
+
+            // Remove first Row that contains the Headers of the CSV
+            rows = rows.Skip(1).ToArray();
+            rows = rows.Take(rows.Count() - 1).ToArray();
+            
+            debugMessage = rows.Length.ToString();
+
+            foreach (string line in rows)
+            {
+                string[] fields = line.Split(';');
+                
+                Vector3 position = new Vector3(float.Parse(fields[1]), float.Parse(fields[2]), float.Parse(fields[3]));
+                Vector3 lookAt = new Vector3(float.Parse(fields[4]), float.Parse(fields[5]), float.Parse(fields[6]));
+
+                UserData ud = new UserData(float.Parse(fields[0]), position, lookAt);
+                userData.Append(ud);
+            }
+            debugMessage = "Load the selected CSV into User Data Object.";
+        }
+        
         if (GUILayout.Button("Show Data"))
         {
             debugMessage = "TODO: Visualize Data in Editor Window";
