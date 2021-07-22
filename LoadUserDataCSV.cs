6c1585e64841745be281ece2b84ea4c28c8f05eb
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-07-22 12:52:17 +0200

    [Add] Simple window layout with debug field

---
+++ LoadUserDataCSV.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LoadUserDataCSV : EditorWindow
+{
+    private string debugMessage = "";
+    
+    // *** BEGIN GUI LAYOUT *** //
+    
+    [MenuItem("Window/Load User Data CSV")] // Add menu item to the Window menu
+    public static void ShowWindow()
+    {
+        // Show existing window instance. If one doesn't exist, make one.
+        EditorWindow.GetWindow(typeof(LoadUserDataCSV));
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Show Data"))
+        {
+            debugMessage = "TODO: Visualize Data in Editor Window";
+        }
+
+        if (GUILayout.Button("Hide Data"))
+        {
+            debugMessage = "TODO: Stop Visualization of Data in Editor Window";
+        }
+        
+        EditorGUILayout.Space();
+        
+        // Editor Window Debug Output
+        EditorGUILayout.HelpBox(debugMessage, MessageType.Warning);
+    }
+}
